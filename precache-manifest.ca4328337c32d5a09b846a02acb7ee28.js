646a1ef94e99e5950661e10ac9251e764bb6e122
Esther Bernal <estherbernal@users-MacBook-Pro.local>
2019-12-03 13:40:52 +0100

    Updates

---
+++ precache-manifest.ca4328337c32d5a09b846a02acb7ee28.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bdd83618798d20ac58232b5310aae7",
+    "url": "/three-test/index.html"
+  },
+  {
+    "revision": "356ff877a4fdf0bc12bc",
+    "url": "/three-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c56a5b23c603fb6d3f12",
+    "url": "/three-test/static/js/2.9967677a.chunk.js"
+  },
+  {
+    "revision": "356ff877a4fdf0bc12bc",
+    "url": "/three-test/static/js/main.e4e2e426.chunk.js"
+  },
+  {
+    "revision": "19e42b0be3e02f486fcf",
+    "url": "/three-test/static/js/runtime-main.2fb93e14.js"
+  },
+  {
+    "revision": "4a1967fb27294f3f6b2ede96c25594e2",
+    "url": "/three-test/static/media/bg-plane.4a1967fb.jpg"
+  },
+  {
+    "revision": "d8934feaf503575aad8227fe53f763f1",
+    "url": "/three-test/static/media/bg01.d8934fea.jpg"
+  },
+  {
+    "revision": "b1d9649596213863fceaef08e7bdb60e",
+    "url": "/three-test/static/media/bg01B.b1d96495.png"
+  },
+  {
+    "revision": "c9964f80009c3786b9b6eb7413d0afcd",
+    "url": "/three-test/static/media/bg02.c9964f80.png"
+  },
+  {
+    "revision": "cb85ae7813b9a04bb22cf80bc2a23318",
+    "url": "/three-test/static/media/bg03.cb85ae78.png"
+  },
+  {
+    "revision": "0323b88d74157fa546808c8af2f84797",
+    "url": "/three-test/static/media/smoke-1.0323b88d.png"
+  }
+]);
